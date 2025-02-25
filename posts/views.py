678698478a88b07ff9d1ff2e0d6ad314c6cd7f66
MD. SHIHAB SHAHRIAR <shahriar@MDs-MacBook-Air-2.local>
2025-02-25 21:55:38 +0600

    Updated

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render,redirect
+from django.urls import reverse_lazy
 from .import forms
 from .import models
+from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView,UpdateView,DeleteView
 # Create your views here.
 
 @login_required
@@ -16,6 +19,19 @@ def add_post(request):
         post_form = forms.PostForm()
     return render(request,'add_post.html',{'form' : post_form})
 
+
+# Add Post using class based view
+@method_decorator(login_required, name='dispatch')
+class AddPostCreateView(CreateView):
+    model = models.Post
+    form_class = forms.PostForm
+    template_name = 'add_post.html'
+    success_url = reverse_lazy('add_post')
+    def form_valid(self, form):
+        form.instance.author = self.request.user   
+        return super().form_valid(form)
+    
+
 @login_required
 def edit_post(request,id):
     post = models.Post.objects.get(pk = id)
@@ -29,8 +45,23 @@ def edit_post(request,id):
 
     return render(request,'add_post.html',{'form' : post_form})
 
+@method_decorator(login_required, name='dispatch')
+class EditPostView(UpdateView):
+    model = models.Post
+    form_class = forms.PostForm
+    template_name = 'add_post.html'
+    pk_url_kwarg = 'id'
+    success_url = reverse_lazy('homepage')
+
 @login_required
 def delete_post(request,id):
     post = models.Post.objects.get(pk=id)
     post.delete()
     return redirect('homepage')
+
+@method_decorator(login_required, name='dispatch')
+class DeletePostView(DeleteView):
+    model = models.Post
+    template_name = 'delete.html'
+    pk_url_kwarg = 'id'
+    success_url = reverse_lazy('profile')
